bc6cb86e13359d4db5a466623658f8bbeaec4f74
kik <kik87827@gmail.com>
2024-06-20 14:44:27 +0900

    템플릿 세팅

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -131,7 +131,7 @@ function mainSwiper(){
   window.addEventListener("resize",()=>{
       heightCheck();
   });
-  $(".scroll_mc .scene_contents").on("wheel scroll",function(e){
+  $(".scene_contents").on("wheel scroll",function(e){
     let thisEvent = $(this);
     if(e.type === "wheel"){
       e.stopPropagation();
@@ -151,6 +151,20 @@ function mainSwiper(){
   });
   $(".btn_top_scroll").on("click",function(){
     mainSwiper.slideTo(0,1000);
+  });
+  $(".nav_top_item , .nav_bottom_item").on("click",function(){
+    let thisIndex = $(this).closest("li").index();
+    if(thisIndex === 0){
+      mainSwiper.slideTo(1,1000);
+    }else if(thisIndex === 1){
+      mainSwiper.slideTo(6,1000);
+    }else if(thisIndex === 2){
+      mainSwiper.slideTo(7,1000);
+    }else if(thisIndex === 3){
+      mainSwiper.slideTo(8,1000);
+    }else if(thisIndex === 4){
+      mainSwiper.slideTo(9,1000);
+    }
   });
 
 
@@ -186,21 +200,31 @@ function mainSwiper(){
   }
 
   function screenAction(){
+    $(".nav_top_list_zone").removeClass("skin2");
     $(".nav_top_item,.nav_bottom_item").removeClass("active");
     //$(".nav_top_list > .nav_bottom_list").find(".nav_top_item,.nav_bottom_item").removeClass("active");
     if (mainSwiper.realIndex == 0) {
-      $(".btn_top_scroll_wrap , .nav_bottom_list_zone").fadeOut();
+        $(".btn_top_scroll_wrap , .nav_bottom_list_zone").fadeOut();
         $(".btn_bottom_scroll_wrap , .nav_top_list_zone").fadeIn();
+    }else if(mainSwiper.realIndex == 9){
+      if($(window).width()>1023){
+        $(".nav_top_list_zone").addClass("skin2").fadeIn();
+        $(".nav_bottom_list_zone ,.btn_top_scroll_wrap").fadeOut();
+        $(".nav_top_item").last().addClass("active");
+      }else{
+        $(".nav_bottom_list_zone , .btn_top_scroll_wrap").fadeIn();
+        $(".nav_bottom_item").last().addClass("active");
+      }
+      
     }else{
-        if (mainSwiper.realIndex >= 1 && mainSwiper.realIndex <= 4) {
-        }else if(mainSwiper.realIndex == 5){
-
+        if (mainSwiper.realIndex >= 1 && mainSwiper.realIndex <= 5) {
+          $(".nav_bottom_item").eq(0).addClass("active");
         }else if(mainSwiper.realIndex == 6){
-
+          $(".nav_bottom_item").eq(1).addClass("active");
         }else if(mainSwiper.realIndex == 7){
-
+          $(".nav_bottom_item").eq(2).addClass("active");
         }else if(mainSwiper.realIndex == 8){
-
+          $(".nav_bottom_item").eq(3).addClass("active");
         }
         $(".btn_top_scroll_wrap , .nav_bottom_list_zone").fadeIn();
         $(".btn_bottom_scroll_wrap , .nav_top_list_zone").fadeOut();
